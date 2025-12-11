e684fedcf3e4eae779690b2eb91458265d9306d2
Elijah Hampton <elijahhamptonj@proton.me>
2025-12-10 19:07:05 -0500

    Cargo fmt, clippy

---
+++ src/utils/mod.rs
@@ -0,0 +1,13 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+/// Returns the current time in millis
+#[allow(clippy::expect_used)]
+pub fn current_time_millis() -> u64 {
+    let dur = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .expect("System time before UNIX epoch!");
+
+    dur.as_secs()
+        .saturating_mul(1_000)
+        .saturating_add(u64::from(dur.subsec_millis()))
+}
